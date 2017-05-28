c7e200ac27007924695ffa417b9938d95a8c4f85
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2017-05-28 12:18:51 +0100

    wallet: add multisig key generation
    Scheme by luigi1111

--- src/simplewallet/simplewallet.h
+++ src/simplewallet/simplewallet.h
@@ -170,6 +170,8 @@ namespace cryptonote
     bool import_outputs(const std::vector<std::string> &args);
     bool show_transfer(const std::vector<std::string> &args);
     bool change_password(const std::vector<std::string>& args);
+    bool prepare_multisig(const std::vector<std::string>& args);
+    bool make_multisig(const std::vector<std::string>& args);
 
     uint64_t get_daemon_blockchain_height(std::string& err);
     bool try_connect_to_daemon(bool silent = false, uint32_t* version = nullptr);
--- src/wallet/wallet2.h
+++ src/wallet/wallet2.h
@@ -315,6 +315,21 @@ namespace tools
       const cryptonote::account_public_address &account_public_address,
       const crypto::secret_key& viewkey = crypto::secret_key());
     /*!
+     * \brief Creates a multisig wallet
+     */
+    void make_multisig(const std::string &password,
+      const std::vector<crypto::secret_key> &view_keys,
+      const std::vector<crypto::public_key> &spend_keys,
+      uint32_t threshold);
+    /*!
+     * Get a packaged multisig information string
+     */
+    std::string get_multisig_info() const;
+    /*!
+     * Verifies and extracts keys from a packaged multisig information string
+     */
+    bool verify_multisig_info(const std::string &data, crypto::secret_key &skey, crypto::public_key &pkey) const;
+    /*!
      * \brief Rewrites to the wallet file for wallet upgrade (doesn't generate key, assumes it's already there)
      * \param wallet_name Name of wallet file (should exist)
      * \param password    Password for wallet file
@@ -384,6 +399,7 @@ namespace tools
     bool testnet() const { return m_testnet; }
     bool restricted() const { return m_restricted; }
     bool watch_only() const { return m_watch_only; }
+    bool multisig(uint32_t *threshold = NULL, uint32_t *total = NULL) const;
 
     uint64_t balance() const;
     uint64_t unlocked_balance() const;
@@ -682,6 +698,9 @@ namespace tools
     std::string seed_language; /*!< Language of the mnemonics (seed). */
     bool is_old_file_format; /*!< Whether the wallet file is of an old file format */
     bool m_watch_only; /*!< no spend key */
+    bool m_multisig; /*!< if > 1 spend secret key will not match spend public key */
+    uint32_t m_multisig_threshold;
+    uint32_t m_multisig_total;
     bool m_always_confirm_transfers;
     bool m_print_ring_members;
     bool m_store_tx_info; /*!< request txkey to be returned in RPC, and store in the wallet cache file */
