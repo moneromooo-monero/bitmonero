5e1f2f70c3b7c664af3adfbb50baeb11047b0b97
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2017-11-02 09:36:45 +0000

    remove seed - 2

--- src/cryptonote_core/cryptonote_tx_utils.cpp
+++ src/cryptonote_core/cryptonote_tx_utils.cpp
@@ -29,7 +29,6 @@
 // Parts of this file are originally copyright (c) 2012-2013 The Cryptonote developers
 
 #include <unordered_set>
-#include <random>
 #include "include_base_utils.h"
 using namespace epee;
 
