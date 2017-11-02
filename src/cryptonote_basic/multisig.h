24197fb5fc7047d04edbde0b35f30e85eeaf6ae8
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2017-11-02 09:35:55 +0000

    multisig.h: headers

--- src/cryptonote_basic/multisig.h
+++ src/cryptonote_basic/multisig.h
@@ -29,7 +29,9 @@
 #pragma once
 
 #include <vector>
+#include <unordered_map>
 #include "crypto/crypto.h"
+#include "cryptonote_basic/cryptonote_format_utils.h"
 #include "ringct/rctTypes.h"
 
 namespace cryptonote
