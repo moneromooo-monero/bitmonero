f2414611888a74f64f916046fcbdf5463f882eb2
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2019-02-01 21:37:36 +0000

    simplewallet: tell the user to complain to the recipient
    for long payment ids

--- src/simplewallet/simplewallet.cpp
+++ src/simplewallet/simplewallet.cpp
@@ -116,7 +116,7 @@ typedef cryptonote::simple_wallet sw;
 #define LONG_PAYMENT_ID_SUPPORT_CHECK() \
   do { \
     if (!m_long_payment_id_support) { \
-      fail_msg_writer() << tr("Long payment IDs are obsolete. Use --long-payment-id-support if you really must use one."); \
+      fail_msg_writer() << tr("Long payment IDs are obsolete. Use --long-payment-id-support if you really must use one, and complain to the recipient about using an obsolete feature that may disappear in the future."); \
       return true; \
     } \
   } while(0)
