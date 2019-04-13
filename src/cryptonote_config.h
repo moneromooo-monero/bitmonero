56c8c8270805e6e74422b891092d147ed17ce928
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2019-04-13 23:37:55 +0000

    core: from v12, require consistent ring size for mixable txes
    We're supposed to have a fixed ring size now
    
    Already checked by MLSAG verification, but here seems more intuitive

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -147,6 +147,7 @@
 #define HF_VERSION_PER_BYTE_FEE                 8
 #define HF_VERSION_SMALLER_BP                   10
 #define HF_VERSION_LONG_TERM_BLOCK_WEIGHT       10
+#define HF_VERSION_SAME_MIXIN                   12
 
 #define PER_KB_FEE_QUANTIZATION_DECIMALS        8
 
