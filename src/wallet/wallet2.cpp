0951c3a9ad0c8376b8abd8908603166d7ffc174d
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2019-06-18 09:28:27 +0000

    wallet: remove long payment ID sending support

--- src/wallet/wallet2.cpp
+++ src/wallet/wallet2.cpp
@@ -1113,7 +1113,6 @@ wallet2::wallet2(network_type nettype, uint64_t kdf_rounds, bool unattended):
   m_first_refresh_done(false),
   m_refresh_from_block_height(0),
   m_explicit_refresh_from_block_height(true),
-  m_confirm_missing_payment_id(true),
   m_confirm_non_default_ring_size(true),
   m_ask_password(AskPasswordToDecrypt),
   m_min_output_count(0),
@@ -3664,9 +3663,6 @@ bool wallet2::store_keys(const std::string& keys_file_name, const epee::wipeable
   value2.SetUint64(m_refresh_from_block_height);
   json.AddMember("refresh_height", value2, json.GetAllocator());
 
-  value2.SetInt(m_confirm_missing_payment_id ? 1 :0);
-  json.AddMember("confirm_missing_payment_id", value2, json.GetAllocator());
-
   value2.SetInt(m_confirm_non_default_ring_size ? 1 :0);
   json.AddMember("confirm_non_default_ring_size", value2, json.GetAllocator());
 
@@ -3860,7 +3856,6 @@ bool wallet2::load_keys(const std::string& keys_file_name, const epee::wipeable_
     m_auto_refresh = true;
     m_refresh_type = RefreshType::RefreshDefault;
     m_refresh_from_block_height = 0;
-    m_confirm_missing_payment_id = true;
     m_confirm_non_default_ring_size = true;
     m_ask_password = AskPasswordToDecrypt;
     cryptonote::set_default_decimal_point(CRYPTONOTE_DISPLAY_DECIMAL_POINT);
@@ -3995,8 +3990,6 @@ bool wallet2::load_keys(const std::string& keys_file_name, const epee::wipeable_
     }
     GET_FIELD_FROM_JSON_RETURN_ON_ERROR(json, refresh_height, uint64_t, Uint64, false, 0);
     m_refresh_from_block_height = field_refresh_height;
-    GET_FIELD_FROM_JSON_RETURN_ON_ERROR(json, confirm_missing_payment_id, int, Int, false, true);
-    m_confirm_missing_payment_id = field_confirm_missing_payment_id;
     GET_FIELD_FROM_JSON_RETURN_ON_ERROR(json, confirm_non_default_ring_size, int, Int, false, true);
     m_confirm_non_default_ring_size = field_confirm_non_default_ring_size;
     GET_FIELD_FROM_JSON_RETURN_ON_ERROR(json, ask_password, AskPasswordType, Int, false, AskPasswordToDecrypt);
@@ -13413,8 +13406,6 @@ std::vector<std::pair<std::string, std::vector<std::tuple<const char*, tools::wa
   v.push_back({"", {std::make_tuple(tr("Refresh type"), std::make_pair(m_refresh_type, std::vector<std::pair<uint32_t, std::string>>({{RefreshFull, "Full"}, {RefreshOptimizeCoinbase, "Optimize coinbase"}, {RefreshNoCoinbase, "No coinbase"}})), SET_enum(RefreshType, m_refresh_type))}});
 
   v.push_back({"", {std::make_tuple(tr("Refresh from block height"), m_refresh_from_block_height, SET(uint64_t, m_refresh_from_block_height))}});
-
-  v.push_back({"", {std::make_tuple(tr("Confirm missing payment ID"), m_confirm_missing_payment_id, SET(bool, m_confirm_missing_payment_id))}});
 
   v.push_back({"", {std::make_tuple(tr("Ask password"), std::make_pair(m_ask_password, std::vector<std::pair<uint32_t, std::string>>({{AskPasswordNever, "Never"}, {AskPasswordOnAction, "Action"}, {AskPasswordToDecrypt, "Decrypt"}})), [&](const configuration_element_t &v0, const password_container &pwd_container) {
     bool changed = false;
