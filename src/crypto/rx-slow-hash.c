9e02bba33cb0db7548a8469e7c82a27b910f6577
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2020-12-05 21:05:21 +0000

    crypto: fix constant randomx exceptions in large page allocator
    If allocating large pages fails, we don't try again.
    This has the obvious drawback of not being able to use large pages
    if they fail once.

--- src/crypto/rx-slow-hash.c
+++ src/crypto/rx-slow-hash.c
@@ -65,6 +65,7 @@ static rx_state rx_s[2] = {{CTHR_MUTEX_INIT,{0},0,0},{CTHR_MUTEX_INIT,{0},0,0}};
 
 static randomx_dataset *rx_dataset;
 static int rx_dataset_nomem;
+static int rx_dataset_nolp;
 static uint64_t rx_dataset_height;
 static THREADV randomx_vm *rx_vm = NULL;
 
@@ -318,9 +319,12 @@ void rx_slow_hash(const uint64_t mainheight, const uint64_t seedheight, const ch
       }
       CTHR_MUTEX_UNLOCK(rx_dataset_mutex);
     }
-    rx_vm = randomx_create_vm(flags | RANDOMX_FLAG_LARGE_PAGES, rx_sp->rs_cache, rx_dataset);
+    rx_vm = NULL;
+    if (!rx_dataset_nolp)
+      rx_vm  = randomx_create_vm(flags | RANDOMX_FLAG_LARGE_PAGES, rx_sp->rs_cache, rx_dataset);
     if(rx_vm == NULL) { //large pages failed
       mdebug(RX_LOGCAT, "Couldn't use largePages for RandomX VM");
+      rx_dataset_nolp = 1;
       rx_vm = randomx_create_vm(flags, rx_sp->rs_cache, rx_dataset);
     }
     if(rx_vm == NULL) {//fallback if everything fails
@@ -370,5 +374,6 @@ void rx_stop_mining(void) {
     randomx_release_dataset(rd);
   }
   rx_dataset_nomem = 0;
+  rx_dataset_nolp = 0;
   CTHR_MUTEX_UNLOCK(rx_dataset_mutex);
 }
