8b9598b49cdc2bacff04bb49b9344dd700b5a23a
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2020-12-08 15:36:54 +0000

    tx_pool: include the original txid when finding a double spend

--- src/cryptonote_core/tx_pool.cpp
+++ src/cryptonote_core/tx_pool.cpp
@@ -1369,7 +1369,7 @@ namespace cryptonote
           }
           if (!meta.double_spend_seen)
           {
-            MDEBUG("Marking " << txid << " as double spending " << itk.k_image);
+            MDEBUG("Marking " << txid << " as double spending " << itk.k_image << ", already spent in " << txid);
             meta.double_spend_seen = true;
             changed = true;
             try
